07933771ca10311c0235f0941f5ac36c47a3b6d0
Bruno-Rizak <brunorizak@gmail.com>
2023-03-02 21:56:26 -0300

    teste

---
+++ aula2/exe05.php
@@ -0,0 +1,18 @@
+<?php
+$var = 15;
+$var2 = 10;
+$var3 = 8;
+$var4 = 5; 
+$soma = $var + $var2 ;
+$sub = $soma - $var4;
+
+
+if ($soma > 20) {
+  $soma  + $var3;
+  echo "$soma" + 8;
+}
+else if (soma <= 20 ) {
+  $sub  - $var4;
+  echo "$sub" - 4;
+}
+?>
