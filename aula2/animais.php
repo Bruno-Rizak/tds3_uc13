cc74b88a6a50dc1a0e7b396de4b3d4c152f4cd2a
Bruno-Rizak <brunorizak@gmail.com>
2023-03-20 21:49:57 -0300

    update

---
+++ aula2/animais.php
@@ -0,0 +1,9 @@
+<?php 
+  $modulo = $_GET['modulo'];
+  if($modulo == "cachorro"){
+    echo "<img src='img/cachorro.png'>";
+  }
+  else if($modulo == "gato"){
+    echo "<img src='img/gato.png'>";
+  }
+?>
